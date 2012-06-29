3b3c8ef3fcc8b95517a8d2608330bf9c6d5cd3e7
Maxim Piskunov <maxitg@neutrino.local>
2012-06-29 12:52:21 +0400

    done task 1

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,16 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Maxim Piskunov on 29.06.2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+- (void)pushOperand:(double)operand;
+- (double)performOperation:(NSString *)operation;
+
+@end
