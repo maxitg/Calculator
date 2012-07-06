17621e2f9095de64111a2704a6ae6ac36d054b68
Maxim Piskunov <maxitg@neutrino.local>
2012-07-06 13:33:07 +0400

    done assignment 2 task 2

--- Calculator/CalculatorBrain.m
+++ Calculator/CalculatorBrain.m
@@ -31,74 +31,195 @@
     return [self.programStack copy];
 }
 
++ (BOOL)isOperation:(NSString *) aString
+{
+    if ([[self arrayOfOperations] containsObject:aString]) return YES;
+    else return NO;
+}
+
++ (NSArray *)arrayOfOperations
+{
+    return [NSArray arrayWithObjects:@"+", @"*", @"-", @"/", @"sin", @"cos", @"sqrt", @"π", @"+ / -", nil];
+}
+
++ (int)numberOfOperationOperands:(NSString *) anOperation
+{
+    if (![self isOperation:anOperation]) return 0;
+    NSArray* operations = [self arrayOfOperations];
+    NSArray* numbersOfOperands = [NSArray arrayWithObjects:
+                                  [NSNumber numberWithInt:2],   //  +
+                                  [NSNumber numberWithInt:2],   //  *
+                                  [NSNumber numberWithInt:2],   //  -
+                                  [NSNumber numberWithInt:2],   //  /
+                                  [NSNumber numberWithInt:1],   //  sin
+                                  [NSNumber numberWithInt:1],   //  cos
+                                  [NSNumber numberWithInt:1],   //  sqrt
+                                  [NSNumber numberWithInt:0],   //  π
+                                  [NSNumber numberWithInt:1],   //  + / -
+                                  nil];
+    return [[[NSDictionary dictionaryWithObjects:numbersOfOperands forKeys:operations] valueForKey:anOperation] intValue];
+}
+
+    //  Lower precedence means this operation should be evaluated first
+
++ (int)objectPrecedence:(NSString *) anOperation
+{
+    if ([anOperation isKindOfClass:[NSNumber class]]) {
+        if ([anOperation doubleValue] < 0) return 3;
+        else return 0;
+    } else if ([self isOperation:anOperation]) {
+        NSArray* operations = [self arrayOfOperations];
+        NSArray* precedences = [NSArray arrayWithObjects:
+                                [NSNumber numberWithInt:2], //  +
+                                [NSNumber numberWithInt:1], //  *
+                                [NSNumber numberWithInt:2], //  -
+                                [NSNumber numberWithInt:1], //  /
+                                [NSNumber numberWithInt:0], //  sin
+                                [NSNumber numberWithInt:0], //  cos
+                                [NSNumber numberWithInt:0], //  sqrt
+                                [NSNumber numberWithInt:0], //  π
+                                [NSNumber numberWithInt:3]  //  + / -
+                                , nil];
+        return [[[NSDictionary dictionaryWithObjects:precedences forKeys:operations] valueForKey:anOperation] intValue];
+    } else {
+        return 0;
+    }
+}
+
++ (int)operationIsCommutative:(NSString *) anOperation
+{
+    NSSet* commutativeOperations = [NSSet setWithObjects:@"+", @"*", nil];
+    return [commutativeOperations containsObject:anOperation];
+}
+
++ (NSString*)descriptionOfTopOfStack:(NSMutableArray*) stack
+{
+    id topOfStack = [stack lastObject];
+    if (topOfStack) [stack removeLastObject];
+    
+    if ([topOfStack isKindOfClass:[NSNumber class]]) {
+        return [topOfStack stringValue];
+    } else if ([topOfStack isKindOfClass:[NSString class]]) {
+        
+        if ([topOfStack isEqualToString:@"+ / -"]) {    //  it is a spectial case
+            int operandPrecedence = [self objectPrecedence:[stack lastObject]];
+            NSString* operand = [self descriptionOfTopOfStack:stack];
+            if (operandPrecedence > 0) {
+                operand = [NSString stringWithFormat:@"(%@)", operand];
+            }
+            
+            return [NSString stringWithFormat:@"-%@", operand];
+        }
+        
+        else if ([self numberOfOperationOperands:topOfStack] == 0) {
+            return topOfStack;
+            
+        } else if ([self numberOfOperationOperands:topOfStack] == 1) {
+            return [NSString stringWithFormat:@"%@(%@)", topOfStack, [self descriptionOfTopOfStack:stack]];
+
+        } else if ([self numberOfOperationOperands:topOfStack] == 2) {
+            int secondOperandPrecedence = [self objectPrecedence:[stack lastObject]];
+            NSString* secondOperand = [self descriptionOfTopOfStack:stack];
+            int firstOperandPrecedence = [self objectPrecedence:[stack lastObject]];
+            
+            BOOL firstOperandIsMinusSomething = NO;
+            if ([self objectPrecedence:[stack lastObject]] == 3) firstOperandIsMinusSomething = YES;
+            
+            NSString* firstOperand = [self descriptionOfTopOfStack:stack];
+            
+            if (!(firstOperandIsMinusSomething && ([self objectPrecedence:topOfStack] == 2)) &&  //  filterning out + / - case
+                (firstOperandPrecedence > [self objectPrecedence:topOfStack])) {   //  example: (2 + 3) * 4
+                firstOperand = [NSString stringWithFormat:@"(%@)", firstOperand];
+            }
+            
+            if ([self operationIsCommutative:topOfStack]) {
+                if (secondOperandPrecedence > [self objectPrecedence:topOfStack]) {  //  example: 4 * (2 + 3) [but not 4 * 2 * 4]
+                    secondOperand = [NSString stringWithFormat:@"(%@)", secondOperand];
+                }
+            } else {
+                if (secondOperandPrecedence >= [self objectPrecedence:topOfStack]) { //  example: 4 / (2 * 3) [4 / 2 * 3 is incorrect]
+                    secondOperand = [NSString stringWithFormat:@"(%@)", secondOperand];
+                }
+            }
+            
+            return [NSString stringWithFormat:@"%@ %@ %@", firstOperand, topOfStack, secondOperand];
+        }
+        
+    }
+    
+    return @"0";
+}
+
 + (NSString*)descriptionOfProgram:(id)program
 {
-    return @"Not yet implemented.";
+    if ([program count] == 0) return nil;
+    NSMutableArray* stack;
+    if ([program isKindOfClass:[NSArray class]]) stack = [program mutableCopy];
+    
+    NSString *description = [self descriptionOfTopOfStack:stack];
+    while ([stack count] != 0) {
+        description = [description stringByAppendingFormat:@", %@", [self descriptionOfTopOfStack:stack]];
+    }
+    
+    return description;
 }
 
 - (void)pushOperand:(double)operand
 {
     [self.programStack addObject:[NSNumber numberWithDouble:operand]];
+    NSLog(@"%@", [[self class] descriptionOfProgram:self.program]);
 }
 
 - (double)performOperation:(NSString *)operation
 {
     [self.programStack addObject:operation];
+    NSLog(@"%@", [[self class] descriptionOfProgram:self.program]);
     return [[self class] runProgram:self.program];
-}
-
-+ (BOOL)isOperation:(NSString *) aString
-{
-    NSSet* operations = [NSSet setWithObjects:@"+", @"*", @"-", @"/", @"sin", @"cos", @"sqrt", @"π", @"+ / -", nil];
-    if ([operations containsObject:aString]) return YES;
-    else return NO;
 }
 
 + (double)popOperandOffProgramStack:(NSMutableArray *) stack
 {
-    double result = 0;
-    
     id topOfStack = [stack lastObject];
     if (topOfStack) [stack removeLastObject];
     
     if ([topOfStack isKindOfClass:[NSNumber class]]) {
-        result = [topOfStack doubleValue];
+        return [topOfStack doubleValue];
     } else if ([topOfStack isKindOfClass:[NSString class]]) {
         
         if ([topOfStack isEqualToString:@"+"]) {
-            result = [self popOperandOffProgramStack:stack] + [self popOperandOffProgramStack:stack];
+            return [self popOperandOffProgramStack:stack] + [self popOperandOffProgramStack:stack];
             
         } else if ([topOfStack isEqualToString:@"*"]) {
-            result = [self popOperandOffProgramStack:stack] * [self popOperandOffProgramStack:stack];
+            return [self popOperandOffProgramStack:stack] * [self popOperandOffProgramStack:stack];
             
         } else if ([topOfStack isEqualToString:@"-"]) {
             double subtrahend = [self popOperandOffProgramStack:stack];
-            result = [self popOperandOffProgramStack:stack] - subtrahend;
+            return [self popOperandOffProgramStack:stack] - subtrahend;
             
         } else if ([topOfStack isEqualToString:@"/"]) {
             double divisor = [self popOperandOffProgramStack:stack];
             double dividend = [self popOperandOffProgramStack:stack];
-            if (divisor) result = dividend / divisor;
+            if (divisor) return dividend / divisor;
             
         } else if ([topOfStack isEqualToString:@"sin"]) {
-            result = sin([self popOperandOffProgramStack:stack]);
+            return sin([self popOperandOffProgramStack:stack]);
             
         } else if ([topOfStack isEqualToString:@"cos"]) {
-            result = cos([self popOperandOffProgramStack:stack]);
+            return cos([self popOperandOffProgramStack:stack]);
             
         } else if ([topOfStack isEqualToString:@"sqrt"]) {
             double operand = [self popOperandOffProgramStack:stack];
-            if (operand >= 0) result = sqrt(operand);
+            if (operand >= 0) return sqrt(operand);
             
         } else if ([topOfStack isEqualToString:@"π"]) {
-            result = M_PI;
+            return M_PI;
             
         } else if ([topOfStack isEqualToString:@"+ / -"]) {
-            result = -[self popOperandOffProgramStack:stack];
+            return -[self popOperandOffProgramStack:stack];
         }
     }
     
-    return result;
+    return 0;
 }
 
 + (double)runProgram:(id)program
@@ -142,12 +263,15 @@
 
 + (NSSet *)variablesUsedInProgram:(id)program
 {
-    return [NSSet setWithArray:[program objectsAtIndexes:[self variableIndexesInProgram:program]]];
+    NSSet* result = [NSSet setWithArray:[program objectsAtIndexes:[self variableIndexesInProgram:program]]];
+    if ([result count] > 0) return result;
+    else return nil;
 }
 
 - (void)clear
 {
     [self.programStack removeAllObjects];
+    NSLog(@"%@", [[self class] descriptionOfProgram:[self program]]);
 }
 
 - (NSString*) description
